de46072c0516372ba181b9408f8207f8329295c1
Nick Towle <ntowle@gmail.com>
2026-09-06 17:58:29 -0400

    Midnight/MurderRow/KystiaManaheart: Add default sound for Fel Spray

--- Midnight/MurderRow/KystiaManaheart.lua
+++ Midnight/MurderRow/KystiaManaheart.lua
@@ -35,7 +35,7 @@ mod:SetRenames({
 
 mod:SetAuraData({
 	{1228198, duration = 30, dispel = "magic", soundOnApplied = "alert", note = CL.debuffPossibleAfterCastNote:format(mod:SpellName(1228198))}, -- Corroding Spittle
-	{1253813, note = CL.debuffFailureMoveFromCastNote:format(mod:SpellName(1253811))}, -- Fel Spray
+	{1253813, soundOnApplied = "underyou", note = CL.debuffFailureMoveFromCastNote:format(mod:SpellName(1253811))}, -- Fel Spray
 })
 
 --------------------------------------------------------------------------------
